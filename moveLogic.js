bdab5d6b2f768bfd83e09f42d10272705dd667a2
tijil g <tijilg1@educbe.ca>
2025-04-17 16:32:35 +0000

    started move logic

--- moveLogic.js
+++ moveLogic.js
@@ -26,15 +26,46 @@ export default function move(gameState){
     // TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
     // gameState.board contains an object representing the game board including its width and height
     // https://docs.battlesnake.com/api/objects/board
-    
+    const boardWidth = gameState.board.width;
+    const boardHeight = gameState.board.height;
+  if(myHead.x === 0) {
+    moveSafety.left = false;
+  }
+  if(myHead.x === boardWidth - 1) {
+    moveSafety.right = false;
+  }
+  if(myHead.y === 0) {
+    moveSafety.down = false;
+  }
+  if(myHead.y === boardHeight - 1) {
+    moveSafety.up = false;
+  }
     // TODO: Step 2 - Prevent your Battlesnake from colliding with itself
     // gameState.you contains an object representing your snake, including its coordinates
     // https://docs.battlesnake.com/api/objects/battlesnake
-    
-    
+
     // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
     // gameState.board.snakes contains an array of enemy snake objects, which includes their coordinates
     // https://docs.battlesnake.com/api/objects/battlesnake
+  const enemies = gameState.board.snakes;
+  enemies.forEach(snake => { 
+  snake.body.forEach(segment => {
+    const { x, y } = segment;
+    if (myHead.x=== x && myHead.y + 1===y) {
+      moveSafety.up = false;
+    }
+    if (myHead.x===x && myHead.y - 1===y) {
+      moveSafety.down = false;
+    }
+    if (myHead.x - 1===x && myHead.y===y) {
+      moveSafety.left = false;
+    }
+    if (myHead.x + 1=== x && myHead.y===y) {
+      moveSafety.right = false;
+    }
+  });
+});
+
     
     // Are there any safe moves left?
     
