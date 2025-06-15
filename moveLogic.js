53f74e0a7f80dd90dc8cf3c9365321056df5f2cc
tijil g <tijilg1@educbe.ca>
2025-06-15 05:23:14 +0000

    done

--- moveLogic.js
+++ moveLogic.js
@@ -44,6 +44,22 @@ export default function move(gameState){
     // gameState.you contains an object representing your snake, including its coordinates
     // https://docs.battlesnake.com/api/objects/battlesnake
 
+    gameState.you.body.slice(1).forEach(segment => {
+    const { x, y } = segment;
+    if (myHead.x === x && myHead.y + 1 === y) {
+        moveSafety.up = false;
+    }
+    if (myHead.x === x && myHead.y - 1 === y) {
+        moveSafety.down = false;
+    }
+    if (myHead.x - 1 === x && myHead.y === y) {
+        moveSafety.left = false;
+    }
+    if (myHead.x + 1 === x && myHead.y === y) {
+        moveSafety.right = false;
+    }
+});
+
     // TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
     // gameState.board.snakes contains an array of enemy snake objects, which includes their coordinates
     // https://docs.battlesnake.com/api/objects/battlesnake
@@ -69,9 +85,7 @@ export default function move(gameState){
     
     // Are there any safe moves left?
     
-    //Object.keys(moveSafety) returns ["up", "down", "left", "right"]
-    //.filter() filters the array based on the function provided as an argument (using arrow function syntax here)
-    //In this case we want to filter out any of these directions for which moveSafety[direction] == false
+
     const safeMoves = Object.keys(moveSafety).filter(direction => moveSafety[direction]);
     if (safeMoves.length == 0) {
         console.log(`MOVE ${gameState.turn}: No safe moves detected! Moving down`);
@@ -83,7 +97,58 @@ export default function move(gameState){
     
     // TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
     // gameState.board.food contains an array of food coordinates https://docs.battlesnake.com/api/objects/board
-    
-    console.log(`MOVE ${gameState.turn}: ${nextMove}`)
-    return { move: nextMove };
+
+
+if (myNeck.x < myHead.x) moveSafety.left = false;
+else if (myNeck.x > myHead.x) moveSafety.right = false;
+else if (myNeck.y < myHead.y) moveSafety.down = false;
+else if (myNeck.y > myHead.y) moveSafety.up = false;
+
+if (myHead.x === 0) moveSafety.left = false;
+if (myHead.x === boardWidth - 1) moveSafety.right = false;
+if (myHead.y === 0) moveSafety.down = false;
+if (myHead.y === boardHeight - 1) moveSafety.up = false;
+
+gameState.you.body.slice(1).forEach(segment => {
+    const { x, y } = segment;
+    if (myHead.x === x && myHead.y + 1 === y) moveSafety.up = false;
+    if (myHead.x === x && myHead.y - 1 === y) moveSafety.down = false;
+    if (myHead.x - 1 === x && myHead.y === y) moveSafety.left = false;
+    if (myHead.x + 1 === x && myHead.y === y) moveSafety.right = false;
+});
+
+gameState.board.snakes.forEach(snake => { 
+    snake.body.forEach(segment => {
+        const { x, y } = segment;
+        if (myHead.x === x && myHead.y + 1 === y) moveSafety.up = false;
+        if (myHead.x === x && myHead.y - 1 === y) moveSafety.down = false;
+        if (myHead.x - 1 === x && myHead.y === y) moveSafety.left = false;
+        if (myHead.x + 1 === x && myHead.y === y) moveSafety.right = false;
+    });
+});
+
+function distance(a, b) {
+    return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+}
+
+const food = gameState.board.food;
+if (food.length > 0) {
+    let closest = food[0];
+    let minDistance = distance(myHead, closest);
+    for (let i = 1; i < food.length; i++) {
+        const d = distance(myHead, food[i]);
+        if (d < minDistance) {
+            closest = food[i];
+            minDistance = d;
+        }
+    }
+    if (closest.x < myHead.x && moveSafety.left) return { move:'left' };
+    if (closest.x > myHead.x && moveSafety.right) return { move:'right' };
+    if (closest.y < myHead.y && moveSafety.down) return { move:'down' };
+    if (closest.y > myHead.y && moveSafety.up) return { move:'up' };
+}
+
+    return { move: safeMoves[Math.floor(Math.random() * safeMoves.length)] };
+
+
 }
